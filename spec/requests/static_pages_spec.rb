b2e0754161943830e40100f01a890064fd16c9d0
David Ynevich <f3game@gmail.com>
2013-07-22 19:33:36 +0400

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+﻿require 'spec_helper'
 
 describe "Static pages" do
 
@@ -31,11 +31,11 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
-    let(:heading)     {'About'}
+    let(:heading)     {'About Us'}
     let(:page_title)  { '' }
 
     it_should_behave_like "all static pages"
-    it { should have_selector 'title', text: '| About' }
+    it { should have_selector 'title', text: '| About Us' }
   end
 
   describe "Contact page" do
@@ -51,13 +51,15 @@ describe "Static pages" do
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About ')
+    page.should have_selector 'title', text: full_title('About Us')
     click_link "Help"
     page.should have_selector 'title', text: full_title('Help')
     click_link "Contact"
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
-    click_link "Sign up now!"
+    click_link "Sign up!"
     page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
    end
 end
